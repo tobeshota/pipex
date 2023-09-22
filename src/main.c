1fdb0c2d9141d61714fb9b60e4b25808ea945d6d
戸部翔太 <tobeshouta@Intel.local>
2023-09-22 13:06:17 +0900

    implement get_cmd_absolute_path_with_option();

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: toshota <toshota@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 17:32:48 by toshota           #+#    #+#             */
-/*   Updated: 2023/09/22 12:28:51 by toshota          ###   ########.fr       */
+/*   Updated: 2023/09/22 13:06:08 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -266,13 +266,13 @@ void get_cmd_name_from_arg(int argc, char **argv, char ***cmd_absolute_path)
 	cmd_absolute_path[0][cmd_i] = NULL;
 }
 
-void get_cmd_option(int argc, char **argv, char ***cmd_absolute_path, char ***cmd_absolute_path_with_option)
+void get_cmd_option(int argc, char **argv, char ***cmd_absolute_path, char ***cmd_option)
 {
 	int arg_i;
 	int cmd_i;
 
-	*cmd_absolute_path_with_option = (char **)malloc(sizeof(char *) * (get_cmd_count(argc, argv) + 1));
-	check_malloc(*cmd_absolute_path_with_option);
+	*cmd_option = (char **)malloc(sizeof(char *) * (get_cmd_count(argc, argv) + 1));
+	check_malloc(*cmd_option);
 	if (is_specified_here_doc(argv))
 		arg_i = 3;
 	else
@@ -282,14 +282,13 @@ void get_cmd_option(int argc, char **argv, char ***cmd_absolute_path, char ***cm
 	{
 		if (!access(argv[arg_i], X_OK) || access(argv[arg_i], F_OK))
 		{
-			cmd_absolute_path_with_option[0][cmd_i] = ft_substr(argv[arg_i], ft_strlen(cmd_absolute_path[0][cmd_i]) + 1, ft_strlen(argv[arg_i]));
-			check_malloc(cmd_absolute_path_with_option[0][cmd_i]);
-		ft_printf("cmd_absolute_path_with_option[0][%d]:\t[%s]\n", cmd_i, cmd_absolute_path_with_option[0][cmd_i]);
+			cmd_option[0][cmd_i] = ft_substr(argv[arg_i], ft_strlen(cmd_absolute_path[0][cmd_i]), ft_strlen(argv[arg_i]));
+			check_malloc(cmd_option[0][cmd_i]);
 			cmd_i++;
 		}
 		arg_i++;
 	}
-	cmd_absolute_path_with_option[0][cmd_i] = NULL;
+	cmd_option[0][cmd_i] = NULL;
 }
 
 void check_cmd(char *env_path)
@@ -434,6 +433,22 @@ void add_absolute_path_to_cmd_name(char ***cmd_absolute_path, char **env_path, c
 	}
 }
 
+void get_cmd_absolute_path_with_option(int argc, char **argv, char ***cmd_option, t_data *data)
+{
+	int cmd_i;
+
+	data->cmd_absolute_path_with_option = (char **)malloc(sizeof(char *) * (get_cmd_count(argc, argv) + 1));
+	check_malloc(data->cmd_absolute_path_with_option);
+	cmd_i = 0;
+	while(data->cmd_absolute_path[cmd_i])
+	{
+		data->cmd_absolute_path_with_option[cmd_i] = ft_strjoin(data->cmd_absolute_path[cmd_i], cmd_option[0][cmd_i]);
+		ft_printf("data->cmd_absolute_path_with_optionn[%d]:\t%s\n", cmd_i, data->cmd_absolute_path_with_option[cmd_i]);
+		cmd_i++;
+	}
+	data->cmd_absolute_path_with_option[cmd_i] = NULL;
+}
+
 /*コマンドライン引数からcmdの絶対パスを取得する
  * ・環境変数のポインタenvpからbin_pathを取得する
  * ・cmdであるべきものを取得する
@@ -442,13 +457,16 @@ void add_absolute_path_to_cmd_name(char ***cmd_absolute_path, char **env_path, c
 void	get_cmd_absolute_path(int argc, char **argv, char **envp, t_data *data)
 {
 	char	**env_path;
+	char	**cmd_option;
 
 	get_env_path(&env_path, envp);
 // env_path = ft_split("PATH=/Library/Frameworks/Python.framework/Versions/3.6/bin/:/Users/tobeshota/anaconda3/condabin/:/opt/homebrew/opt/node@18/bin/:/Users/tobeshota/.cargo/bin/:/usr/local/Qt-5.15.10/bin/:/opt/homebrew/opt/pyqt@5/5 5.15.7_2/bin/:/opt/homebrew/opt/qt@5/bin/:/Users/tobeshota/.nodebrew/current/bin/:/Users/tobeshota/.pyenv/shims/:/Users/tobeshota/.pyenv/bin/:/Library/Frameworks/Python.framework/Versions/3.10/bin/:/usr/local/bin/:/usr/bin/:/bin/:/usr/sbin/:/sbin/:/opt/X11/bin/:/Users/tobeshota/workspace/command", ':');
 	get_cmd_name_from_arg(argc, argv, &data->cmd_absolute_path);
-	get_cmd_option(argc, argv, &data->cmd_absolute_path, &data->cmd_absolute_path_with_option);
+	get_cmd_option(argc, argv, &data->cmd_absolute_path, &cmd_option);
 	add_absolute_path_to_cmd_name(&data->cmd_absolute_path, env_path, envp);
+	get_cmd_absolute_path_with_option(argc, argv, &cmd_option, data);
 	all_free(env_path);
+	all_free(cmd_option);
 }
 
 void proc_here_doc(char *limitter, int infile_fd)
