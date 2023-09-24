f0d50ecb21cca3bbda285c21fb82d4ea891e688b
戸部翔太 <tobeshouta@Intel.local>
2023-09-24 11:28:40 +0900

    delete comment

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: toshota <toshota@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 17:32:48 by toshota           #+#    #+#             */
-/*   Updated: 2023/09/24 02:49:23 by toshota          ###   ########.fr       */
+/*   Updated: 2023/09/24 11:23:43 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -164,7 +164,6 @@ void	check_malloc(void *ptr)
 	}
 }
 
-// pathそれぞれの文字列の終わりに"/"を加える．
 void add_slash_eos(char ***path)
 {
 	char *tmp;
@@ -182,7 +181,6 @@ void add_slash_eos(char ***path)
 
 int is_cmd_alreadly_absollute_path(char ***cmd_absolute_path, int cmd_i)
 {
-	// "/"が文頭にあり，かつ，"/"が文中にあるならば，それは絶対パスである
 	if (cmd_absolute_path[0][cmd_i][0] == '/' && ft_strchr(&cmd_absolute_path[0][cmd_i][1], '/'))
 		return TRUE;
 	return FALSE;
@@ -216,7 +214,6 @@ void	get_pwd(char ***pwd_path, char **envp)
 	add_slash_eos(pwd_path);
 }
 
-// コマンドであるべきコマンドライン引数の数を取得する
 int get_cmd_count(int argc, char **argv)
 {
 	int cmd_count;
@@ -229,11 +226,6 @@ int get_cmd_count(int argc, char **argv)
 		i = 2;
 	while (i < argc - 1)
 	{
-		// argv[i]は存在する読み取りファイル，存在する書き込みファイル，存在する実行ファイルのいずれかである．
-		// argv[i]が実行可能であるとき，それは実行ファイルである．
-		// また，パスが省略されうる（絶対パスや相対パスで指定されるという意味ではない）ファイルは実行ファイルのみである．
-		// ゆえに，パスが省略されたために存在していないと評価されたファイルは，実行ファイルでなければならない
-		// したがって，argv[i]が実行ファイルであるかを調べるには，それが実行可能であるか，パスが省略されたために存在しないと評価されるかを調べれば良い．
 		if (!access(argv[i], X_OK) || access(argv[i], F_OK))
 			cmd_count++;
 		i++;
@@ -250,10 +242,6 @@ size_t strlen_until_c(char *str, char c)
 	return len;
 }
 
-/* コマンドライン引数からcmdであるべきものを取得する
- * "here_doc"がない場合，argv[0](実行ファイル)，argv[1](infile)，argv[argc-1](outfile)以外を「cmdであるべきもの」として取得する
- * "here_doc"がある場合，argv[0](実行ファイル)，argv[1](here_doc)，argv[2](LIMITTER)，argv[argc-1](outfile)以外であり，かつ，F_OKに失敗したものを「cmdであるべきもの」として取得する
- */
 void get_cmd_name_from_arg(int argc, char **argv, char ***cmd_absolute_path)
 {
 	int arg_i;
@@ -315,7 +303,6 @@ void check_cmd(char *env_path)
 
 int is_cmd_relative_path(char ***cmd_absolute_path, int cmd_i)
 {
-	// "/"が文頭になく，かつ，"/"が文中にあるならば，それは相対パスである．
 	if (cmd_absolute_path[0][cmd_i][0] != '/' && ft_strchr(&cmd_absolute_path[0][cmd_i][1], '/'))
 		return TRUE;
 	return FALSE;
@@ -334,7 +321,6 @@ int get_down_count_from_pwd(char *relative_path)
 	return down_count_from_pwd;
 }
 
-// 文字列 s 中に n 番目に文字 c が現れた位置へのポインターを返す
 char	*ft_strrnchr(const char *s, int c, int n)
 {
 	char	ch;
@@ -358,8 +344,6 @@ char	*ft_strrnchr(const char *s, int c, int n)
 	return (0);
 }
 
-// PWDから何段下がるかの数ぶんPWDを変更する
-// 文末からn個'/'が来るまでpwd_pathを消す
 char *get_pwd_for_relative_path(char ***pwd_path, int down_count_from_pwd)
 {
 	int delete_len;
@@ -379,12 +363,6 @@ void delete_relative_path(char ***cmd_absolute_path, int cmd_i)
 	free(tmp);
 }
 
-/* 相対パスから絶対パスに変換する
- * ・PWDを取得する
- * ・PWDを相対パスで指定された段数ぶん変更する
- * ・cmd_absolute_pathから相対パスの部分（"../"や"./"）を削除する
- * ・cmd_absolute_pathにpathを加える
- */
 void convert_relative_path_to_absolute_path(char ***cmd_absolute_path, int cmd_i, char **envp)
 {
 	char	*tmp;
@@ -423,11 +401,6 @@ void add_absolute_path_to_cmd_name_from_env_path(char ***cmd_absolute_path, char
 	check_cmd(env_path[env_i]);
 }
 
-/* cmdにパスを加える
- * cmd一つ一つに対して，にenv_pathを一つずつ結合していく．
- * 結合したものが実行可能であるならば，それをコマンドの絶対パスとして返す.
- * 実行可能なenv_pathが見つからなければ，エラーとする．
- */
 void add_absolute_path_to_cmd_name(char ***cmd_absolute_path, char **env_path, char **envp)
 {
 	int cmd_i;
@@ -461,11 +434,6 @@ void get_cmd_absolute_path_with_option(int argc, char **argv, char ***cmd_option
 	data->cmd_absolute_path_with_option[cmd_i] = NULL;
 }
 
-/*コマンドライン引数からcmdの絶対パスを取得する
- * ・環境変数のポインタenvpからbin_pathを取得する
- * ・cmdであるべきものを取得する
- * ・cmdにパスを加える
- */
 void	get_cmd_absolute_path(int argc, char **argv, char **envp, t_data *data)
 {
 	char	**env_path;
@@ -510,11 +478,6 @@ void proc_here_doc(char *limitter, int infile_fd)
 	infile_fd = open_file(HERE_DOC_FILE_PATH, INFILE_HERE_DOC);
 }
 
-/* ■ファイルおよびコマンドは適切なものであるかを確かめる
- * 	・入力用ファイルは読み取り可能であり，かつ，ディレクトリでないかを確かめる
- * 	・出力用ファイルは書き込み可能であり，かつ，ディレクトリでないかを確かめる
- * 	・コマンドが存在するかを確かめる（command not foundとならないかを調べる）
- */
 int	is_argv_valid(int argc, char **argv)
 {
 	if (is_infile_valid(get_infile(argv)) == FALSE)
@@ -543,10 +506,6 @@ void get_outfile_fd(int argc, char **argv, int *outfile_fd)
 		*outfile_fd = open_file(get_outfile(argc, argv), OUTFILE);
 }
 
-/* コマンドライン引数が適切であるかを確かめる
- * ・コマンドライン引数の数は5個以上あるかを確かめる
- * ・コマンドライン引数の要素は適切なものであるかを確かめる
- */
 void check_arg(int argc, char **argv)
 {
 	if (is_argc_valid(argc, argv) == FALSE)
@@ -569,11 +528,10 @@ void get_pipe(t_data *data, int *cmd_i)
 	int ret;
 
 	ret = pipe(data->pipe_fd[*cmd_i]);
-	// ret = pipe(data->p_fd);
 	check_pipe(ret);
 }
 
-void get_multiple_pipe(int argc, char **argv, t_data *data)
+void malloc_multiple_pipe(int argc, char **argv, t_data *data)
 {
 	int	i;
 
@@ -594,14 +552,13 @@ void	get_data(int argc, char **argv, char **envp, t_data *data)
 	get_cmd_absolute_path(argc, argv, envp, data);
 	get_infile_fd(argv, &data->infile_fd);
 	get_outfile_fd(argc, argv, &data->outfile_fd);
-	get_multiple_pipe(argc, argv, data);
+	malloc_multiple_pipe(argc, argv, data);
 }
 
 void set_input_fd(t_data *data, int *cmd_i)
 {
 	if (*cmd_i == 0)
 	{
-		// [I1]さいしょの入力先では，標準入力をinfile_fdにする
 		if (dup2(data->infile_fd, STDIN_FILENO) == -1)
 		{
 			put_error("failed to dup\n");
@@ -611,7 +568,6 @@ void set_input_fd(t_data *data, int *cmd_i)
 	}
 	else
 	{
-		// [I2]2回目以降の入力先では，標準入力(infile_fd)をp_fd[0]する
 		if (dup2(data->pipe_fd[*cmd_i - 1][0], STDIN_FILENO) == -1)
 		{
 			put_error("failed to dup\n");
@@ -626,7 +582,6 @@ void set_output_fd(t_data *data, int *cmd_i)
 {
 	if (data->cmd_absolute_path[*cmd_i + 1] != NULL)
 	{
-		// [O1]さいごより1回前の出力先では，標準出力をp_fd[1]にする
 		if (dup2(data->pipe_fd[*cmd_i][1], STDOUT_FILENO) == -1)
 		{
 			put_error("failed to dup\n");
@@ -637,7 +592,6 @@ void set_output_fd(t_data *data, int *cmd_i)
 	}
 	else
 	{
-		// [O2]さいごの出力先では，標準出力(data->p_fd[1])をoutfile_fdにする
 		if (dup2(data->outfile_fd, STDOUT_FILENO) == -1)
 		{
 			put_error("failed to dup\n");
@@ -738,14 +692,3 @@ int	main(int argc, char **argv, char **envp)
 	pipex(envp, &data);
 	end_pipex(argv, &data);
 }
-
-/*
-gcc -g main.c ../libft/libft.a -o pipex
-cc -Wall -Wextra -Werror main.c ../libft/libft.a -o pipex
-
-./pipex infile cat sort outfile
-./pipex infile "ls -a" cat outfile
-./pipex infile "ls -a" cat ../../a.out outfile
-./pipex infile "ls -a" brew /bin/cat ../../a.out fuga/a.out outfile
-./pipex here_doc wow cat sort outfile
-*/
